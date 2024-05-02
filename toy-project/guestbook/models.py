56c8b9cfdeecde3670c88a41f1b2dda80ab49d8e
cherrynniii <chaelin20760@gmail.com>
2024-05-02 23:14:22 +0900

    feat : guestbook의 models.py 모델 추가

--- toy-project/guestbook/models.py
+++ toy-project/guestbook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Post(BaseModel):
+
+    id = models.AutoField(primary_key=True)
+    writer = models.CharField(verbose_name="작성자", max_length=10)
+    title = models.CharField(verbose_name="제목", max_length=20)
+    content = models.TextField(verbose_name="내용")
+    password = models.CharField(max_length=20, verbose_name="비밀번호", default=None, null=True)
