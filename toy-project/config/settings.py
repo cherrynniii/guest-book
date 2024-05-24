ae48bd7cd464a0496d8bd87a86de9e987cd05492
cherrynniii <chaelin20760@gmail.com>
2024-05-25 01:10:18 +0900

    docs : settings.py cors 접근 권한 추가

--- toy-project/config/settings.py
+++ toy-project/config/settings.py
@@ -68,6 +68,7 @@ THIRD_PARTY_APPS = [
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -156,4 +157,5 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:9001"
 ]
